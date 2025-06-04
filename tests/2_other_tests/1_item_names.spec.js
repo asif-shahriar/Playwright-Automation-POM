6a40408b9a1898097919ea211f8d529b0d22985e
asif-shahriar <shahriarasif5@gmail.com>
2025-06-04 16:40:55 +0600

    add tag keyword in test description

--- tests/2_other_tests/1_item_names.spec.js
+++ tests/2_other_tests/1_item_names.spec.js
@@ -1,7 +1,7 @@
 import { test } from '../../global_setup/test_setup.js';
 import { HomePage } from '../../pages/homePage.js'
 
-test('Print all item names', async ({ page }) => {
+test('[regression] [smoke] Print all item names', async ({ page }) => {
     const homePage = new HomePage(page)
     await homePage.printAllItemNames()
 })
--- tests/2_other_tests/2_link_texts.spec.js
+++ tests/2_other_tests/2_link_texts.spec.js
@@ -1,7 +1,7 @@
 import { test } from '../../global_setup/test_setup.js';
 import { HomePage } from '../../pages/homePage.js'
 
-test('Print all unique link texts', async ({ page }) => {
+test('[regression] Print all unique link texts', async ({ page }) => {
     const homePage = new HomePage(page)
     await homePage.allUniqueLinkText()
 })
